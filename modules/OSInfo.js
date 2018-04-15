80c01a647433c6d7d9bc7a64c0af71e7516c9727
Piotr Pidlypczak <peterpid@gmail.com>
2018-04-15 21:57:35 +0200

    Add colors

--- modules/OSInfo.js
+++ modules/OSInfo.js
@@ -1,5 +1,6 @@
-var os   = require('os');
-var time = require('./time');
+var os     = require('os');
+var colors = require('colors');
+var time   = require('./time');
 
 function getOSinfo() {
     var type = os.type();
@@ -12,17 +13,17 @@ function getOSinfo() {
     var cpu = os.cpus()[0].model;
     var uptime = os.uptime();
     var userInfo = os.userInfo();
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
+    console.log('System:'.gray, type);
+    console.log('Release:'.red.dim, release);
+    console.log('CPU model:'.blue, cpu);
     let days = time.toDays(uptime);
     let hours = time.toHours(uptime) % 24;
     let minutes = time.toMinutes(uptime) % 60;
     let uptimeStr = (days > 0)?days + 'd':'';
     uptimeStr += (hours  > 0)?' ' + hours + 'h':'';
     uptimeStr += (minutes > 0)?' ' + minutes + 'min':'';
-    console.log('Uptime: ', uptimeStr);
-    console.log('User name:', userInfo.username);
+    console.log('Uptime:'.green.dim, uptimeStr);
+    console.log('User name:'.yellow.dim, userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
 
